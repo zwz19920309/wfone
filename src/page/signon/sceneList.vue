655211d503e4c7176e305aee7089789f439eba2b
钟文治 <13824443763@qq.com>
2019-04-16 22:10:47 +0800

    add

--- src/page/signon/sceneList.vue
+++ src/page/signon/sceneList.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="fillcontain">
     <div class="table_container">
-      <div class="mar10">
+      <!-- <div class="mar10">
         <div class="pad10">
           <el-table border :data="sceneList" stripe style="width: 100%">
             <el-table-column prop="name" label="名称"  width="180"></el-table-column>
             <el-table-column prop="note" label="描述"></el-table-column>
-            <!-- <el-table-column prop="desc" label="活动详情" width="180">
+            <el-table-column prop="desc" label="活动详情" width="180">
               <template>
                 <span><a>签到活动列表</a></span>
               </template>
-             </el-table-column> -->
+             </el-table-column> 
             <el-table-column prop="createdAt" label="创建时间"></el-table-column>
             <el-table-column prop="desc" label="签到活动" width="180">
               <template slot-scope="scope">
@@ -23,24 +23,29 @@
       </div>
       <div>
         <signon-list-dialog :signonList="signonList" :callBack="changeScenesign" ref="signonListRef"></signon-list-dialog>
+      </div> 
+      -->
+      <div>
+        <scene-list :sceneList="sceneList"  :isEdit="isEdit" ref="sceneListRef"></scene-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { getSceneList, getSignonListBySceneId, bulkAddScenesign, bulkDeleteScenesign } from '@/api/getData'
+import { getSceneList } from '@/api/getData'
 export default {
   data () {
     return {
       scene: {},
       handleType: 0,
       sceneList: [],
-      signonList: []
+      signonList: [],
+      isEdit: true
     }
   },
   components: {
-    'signon-list-dialog': () => import('@/components/signonListDialog.vue')
+    'scene-list': () => import('@/components/sceneList.vue')
   },
   created () {
     this.initData()
@@ -51,40 +56,6 @@ export default {
       console.log('@data: ', res)
       if ((res.code === 0) && (res.data.list.length)) {
         this.sceneList = res.data.list
-      }
-    },
-    async handleSigonList (index, row, type) {
-      this.scene = row
-      this.handleType = type
-      let res = await getSignonListBySceneId({ sceneId: this.scene.id, type: this.handleType })
-      if ((res.code === 0) && (res.data.list.length)) {
-        this.signonList = res.data.list
-      } else {
-        if (this.handleType === 1) {  
-          this.$message.error('暂无新增签到活动模板')
-        } else {
-          this.$message.error('该场景尚未配置签到活动模板')
-        }
-        return
-      }
-      this.$refs.signonListRef.open()
-    },
-    async changeScenesign (signonList) {
-      let scenesignons = []
-      signonList.forEach(signon => {
-        scenesignons.push([ this.scene.id, signon.id ])
-      })
-      let res
-      if (this.handleType === 1) {
-        res = await bulkAddScenesign({ scenesignons: scenesignons })
-      } else {
-        res = await bulkDeleteScenesign({ scenesignons: scenesignons })
-      }
-      if (res && res.code === 0) {
-        this.$message({ message: '操作成功', type: 'success' })
-        this.$refs.signonListRef.close()
-      } else {
-        this.$message.error('操作失败')    
       }
     }
   }
